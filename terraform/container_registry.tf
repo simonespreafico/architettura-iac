0640d2aabf15645b8531e79e0355002709ac0757
Simone <simone.spreafico@certimetergroup.com>
2023-12-01 14:34:16 +0100

    aggiunti container registry ECR

--- terraform/container_registry.tf
+++ terraform/container_registry.tf
@@ -1,3 +1,37 @@
-/* Container registry ECR */
+/* Containers repos ECR */
 
+resource "aws_ecr_repository" "gateway" {
+  name                 = "application/gateway"
+  image_tag_mutability = "MUTABLE"
 
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "user" {
+  name                 = "application/user"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "certificate" {
+  name                 = "application/certificate"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "frontend" {
+  name                 = "application/frontend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
