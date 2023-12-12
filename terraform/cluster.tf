c703ca3f0f87baef381f0ba08419da9d0f2c758c
Simone <simone.spreafico@certimetergroup.com>
2023-12-12 10:55:07 +0100

    rimosso node-group-2 da cluster eks

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -26,16 +26,6 @@ module "eks" {
       max_size     = 3
       desired_size = 2
     }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
   }
 }
 
