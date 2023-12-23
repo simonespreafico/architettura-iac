4cf1923e49f254019e43d08c968072d0836f75f2
Simone <simone.spreafico@certimetergroup.com>
2023-12-23 19:33:29 +0100

    aggiunti registri container ecr

--- terraform/container_registry.tf
+++ terraform/container_registry.tf
@@ -49,6 +49,60 @@ resource "aws_ecr_repository_policy" "repository_frontend_policy" {
   policy     = data.aws_iam_policy_document.ecr_policy.json
 }
 
+resource "aws_ecr_repository_policy" "repository_question_policy" {
+  repository = aws_ecr_repository.question.name
+  policy     = data.aws_iam_policy_document.ecr_policy.json
+}
+
+resource "aws_ecr_repository_policy" "repository_template_policy" {
+  repository = aws_ecr_repository.template.name
+  policy     = data.aws_iam_policy_document.ecr_policy.json
+}
+
+resource "aws_ecr_repository_policy" "repository_quiz_policy" {
+  repository = aws_ecr_repository.quiz.name
+  policy     = data.aws_iam_policy_document.ecr_policy.json
+}
+
+resource "aws_ecr_repository" "quiz" {
+  name                 = "application/quiz"
+  image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "template" {
+  name                 = "application/template"
+  image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "question" {
+  name                 = "application/question"
+  image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 resource "aws_ecr_repository" "gateway" {
   name                 = "application/gateway"
   image_tag_mutability = "MUTABLE"
